1b83cb48c8d74b86196cfcdc276de6e79b2f9cbd
Patrick Sheriddan <sheridp2@gmail.com>
2020-12-15 14:00:19 -0800

    Id generatable and saved to local storage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Login from './Login'
+import useLocalStorage from '../hooks/useLocalStorage'
+import Dashboard from './Dashboard'
 
 function App() {
+  const[id, setId] = useLocalStorage()
+
   return (
     <div>
-      <Login />
+      {id ? <Dashboard id={id} /> : <Login onIdSubmit={setId}/>}
     </div>
   );
 }
