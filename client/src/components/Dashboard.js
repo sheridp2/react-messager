39ff95a6d4334e59b28e5d2c1edb022c83489d26
Patrick Sheriddan <sheridp2@gmail.com>
2020-12-16 13:04:16 -0800

    conversation and contacts components created and toggleable between

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import Sidebar from './Sidebar'
 
 function Dashboard({ id }) {
   return (
-    <div>
-      {id}
+    <div className="d-flex" style={{ height: '100vh'}}>
+      <Sidebar id={id} />
     </div>
   )
 }
